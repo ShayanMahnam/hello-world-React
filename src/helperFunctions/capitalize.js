6195a853bc97a015b366803357c86092986f4350
Shayan Mahnam <shayan.mahnam.uk@gmail.com>
2023-02-17 13:23:53 +0000

    update capitalize

--- src/helperFunctions/capitalize.js
+++ src/helperFunctions/capitalize.js
@@ -1,4 +1,13 @@
 const capitalize = (str) => {
+  if(str.includes(' ')){
+    return str
+      .split(" ")
+      .map(
+        (element) =>
+          element.charAt(0).toUpperCase() + element.slice(1).toLowerCase()
+      ).join(" ")
+
+  }
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 };
 
